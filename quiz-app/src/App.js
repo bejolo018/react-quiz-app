cb6bf0bc5e1b13c106d6d3234bce3e7cd5a27690
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-01 18:39:03 -0500

    styled cards and implemented transform and transition elements for card flipping animation

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FlashcardList from './Components/FlashcardList';
+import './app.css'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASH_CARDS)

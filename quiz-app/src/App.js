9c1593b7c3c5b4ce185a13112c706d23882205dd
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-01 18:08:24 -0500

    created components, using dummy data to setup component heiarchy

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './Components/FlashcardList';
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASH_CARDS)
+
   return (
-    <div className="App">
-        
+    <div>
+        <FlashcardList flashcards={flashcards}/>
     </div>
   );
 }
+
+const SAMPLE_FLASH_CARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'What is 2 + 3?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+]
 
 export default App;

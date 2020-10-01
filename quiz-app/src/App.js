39d364c0cc3a941922d4b70c2a52d9a86aa8a301
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-01 18:55:24 -0500

    Used useEffect hook and axios to use data from the opentdb api

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './Components/FlashcardList';
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASH_CARDS)
+
+  useEffect(() => {
+    axios
+
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res =>{
+
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+        console.log(res.data)
+      })
+  }, [])
+
+  function decodeString(str){
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
 
   return (
     <div>
